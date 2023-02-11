acc3edbbf493d5bc5bf70bc423925f56cc3ff5de
AntonPing <antonping1999@gmail.com>
2023-02-11 17:32:31 +0300

    feat: add command line interface
    - add file `lib.rs` and move all modules in `main.rs` into this file
    - add crate dependancy `clap = "4.1.4"` in `Cargo.toml`
    - implement command line interface in `main.rs` using `clap`

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+pub mod ast;
+pub mod intern;
+pub mod lexer;
+pub mod parser;
+pub mod position;
+pub mod printer;
+pub mod env_map;
+pub mod renamer;
+pub mod infer;
+pub mod diagnostic;
+pub mod anf;
+pub mod visitor;
+pub mod normalize;
+pub mod simple_opt;
+pub mod clos_conv;
+pub mod codegen;
+pub mod driver;
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,133 @@
-pub mod ast;
-pub mod intern;
-pub mod lexer;
-pub mod parser;
-pub mod position;
-pub mod printer;
-pub mod env_map;
-pub mod renamer;
-pub mod infer;
-pub mod diagnostic;
-pub mod anf;
-pub mod visitor;
-pub mod normalize;
-pub mod simple_opt;
-pub mod clos_conv;
-pub mod codegen;
+use norem::driver;
 
 fn main() {
-    println!("Hello, world!");
+    use std::path::PathBuf;
+    extern crate clap;
+    use clap::{Arg, ArgAction, Command};
+
+    let matches = Command::new("norem")
+        .version("0.1.0")
+        .author("Anton Ping <antonping1999@gmail.com>")
+        .about(
+            "\
+            A native-compiled functional programming language.\
+            The word \"norem\" stands for \"not really monadic\".\
+        ",
+        )
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("compile")
+                .about("compile norem source file to target language")
+                .arg(
+                    Arg::new("INPUT")
+                        .required(true)
+                        .help("path of input norem source file"),
+                )
+                .arg(
+                    Arg::new("OUTPUT")
+                        .short('o')
+                        .long("output")
+                        .required(false)
+                        .help("path for saving output file"),
+                )
+                .arg(
+                    Arg::new("DUMP")
+                        .short('d')
+                        .long("dump")
+                        .required(false)
+                        .action(ArgAction::SetTrue)
+                        .help("print intermediate result of compiliation"),
+                ),
+        )
+        .subcommand(
+            Command::new("link")
+                .about("link compiled norem file with external library")
+                .arg(
+                    Arg::new("CODE")
+                        .required(true)
+                        .help("path of compiled norem file"),
+                )
+                .arg(
+                    Arg::new("LIBRARY")
+                        .required(true)
+                        .help("path of external library"),
+                )
+                .arg(
+                    Arg::new("OUTPUT")
+                        .short('o')
+                        .long("output")
+                        .required(false)
+                        .help("path for saving output file"),
+                ),
+        )
+        .get_matches();
+
+    match matches.subcommand().unwrap() {
+        ("compile", sub_matches) => {
+            let input: PathBuf = sub_matches
+                .get_one::<String>("INPUT")
+                .map(|x| x.into())
+                .unwrap();
+
+            if !matches!(input.extension(), Some(x) if x == "nrm") {
+                panic!("norem source name file should end with '.nrm'!");
+            }
+
+            let output: PathBuf = sub_matches
+                .get_one::<String>("OUTPUT")
+                .map(|x| x.into())
+                .unwrap_or(PathBuf::from("output.c"));
+
+            if !matches!(output.extension(), Some(x) if x == "c") {
+                panic!("output file name should end with '.c'!");
+            }
+
+            let dump = sub_matches.get_flag("DUMP");
+            match driver::run_compile(&input, &output, dump) {
+                Ok(()) => {
+                    println!("compilation successed.");
+                }
+                Err(err) => {
+                    println!("{err}");
+                    println!("compilation failed!");
+                }
+            }
+        }
+        ("link", sub_matches) => {
+            let code: PathBuf = sub_matches
+                .get_one::<String>("CODE")
+                .map(|x| x.into())
+                .unwrap();
+
+            if !matches!(code.extension(), Some(x) if x == "c") {
+                panic!("compiled code file name should end with '.c'!");
+            }
+
+            let library: PathBuf = sub_matches
+                .get_one::<String>("LIBRARY")
+                .map(|x| x.into())
+                .unwrap();
+
+            if !matches!(library.extension(), Some(x) if x == "c") {
+                panic!("external library file name should end with '.c'!");
+            }
+
+            let output: PathBuf = sub_matches
+                .get_one::<String>("OUTPUT")
+                .map(|x| x.into())
+                .unwrap_or(PathBuf::from("output.out"));
+
+            match driver::run_link(&code, &library, &output) {
+                Ok(()) => {
+                    println!("linking successed.");
+                }
+                Err(err) => {
+                    println!("{err}");
+                    println!("linking failed!");
+                }
+            }
+        }
+        _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
+    }
 }
